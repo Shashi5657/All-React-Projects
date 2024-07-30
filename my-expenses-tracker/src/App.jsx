1e89e24533cdf3d595909540b367e1186d529479
Shashi5657 <sangepushashidhar@gmail.com>
2024-07-30 19:24:16 +0530

    commit message

--- my-expenses-tracker/src/App.jsx
+++ my-expenses-tracker/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h2>Expense Tracker</h2>
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
